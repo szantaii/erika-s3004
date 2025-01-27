67e9fefee5ac2c80a32aca9400b09e89895e6db7
szantaii <szantaii@gmail.com>
2025-01-27 18:34:39 +0100

    Remove cellular automaton related cli parser

--- erika/cli.py
+++ erika/cli.py
@@ -1,5 +1,5 @@
 import argparse
-from typing import Optional, Union
+from typing import Optional
 
 
 class ErikaCli:
@@ -31,46 +31,3 @@ class ErikaCli:
 
     def parse_args(self) -> dict[str, str]:
         return vars(self._parser.parse_args())
-
-
-class InitialStatesAction(argparse.Action):
-    def __call__(self, parser, namespace, values, option_string=None):
-
-        if values is None:
-            setattr(namespace, self.dest, values)
-
-            return
-
-        setattr(
-            namespace,
-            self.dest,
-            [int(char) for char in values if char in '01']
-        )
-
-
-class ErikaAutomatonCli(ErikaCli):
-    def __init__(self):
-        super().__init__()
-
-        self._parser.add_argument(
-            '-r', '--rule',
-            required=True,
-            type=int,
-            # help='TODO',
-            metavar='110'
-        )
-        self._parser.add_argument(
-            '-s', '--initial-states',
-            required=False,
-            default=None,
-            action=InitialStatesAction,
-            # help='TODO',
-            metavar='01010110100111110101011000011101'
-        )
-
-    def parse_args(self) -> dict[str, Union[str, Optional[list[int]]]]:
-        return {
-            k: vars(self._parser.parse_args())[k]
-            for k
-            in ['device', 'rule', 'language', 'initial_states']
-        }
